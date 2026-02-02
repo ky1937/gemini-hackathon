bae301141027ab200879207cb14531d10a9e2527
kaiyang1937 <kytan03@@gmail.com>
2026-02-02 19:27:58 +0800

    interface version

--- src/main/java/com/hackathon/spmguidance/controller/ChatController.java
+++ src/main/java/com/hackathon/spmguidance/controller/ChatController.java
@@ -1,13 +1,18 @@
 package com.hackathon.spmguidance.controller;
 
-import com.hackathon.spmguidance.dto.ChatRequest;
-import com.hackathon.spmguidance.dto.ChatResponse;
-import com.hackathon.spmguidance.service.GeminiService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hackathon.spmguidance.dto.ChatRequest;
+import com.hackathon.spmguidance.dto.ChatResponse;
+import com.hackathon.spmguidance.service.GeminiService;
 
 @RestController
 @RequestMapping("/api")
